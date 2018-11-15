9e6e2adcf4cae10db5bd47d500ab1028a27ae588
YuJames <7332798+YuJames@users.noreply.github.com>
2018-11-15 15:38:37 -0600

    new print function
    added a print function that repeats an integer

---
+++ coolprint.py
@@ -0,0 +1,13 @@
+def repeat_print_3(num):
+    """Repeatedly print out a positive integer 3 times.
+    
+    Args:
+        num: what to print (int)
+    Returns:
+        None
+    """
+    
+    for _ in range(3):
+        print(num)
+        
+repeat_print_3(4)
